b3512a7885265814c543f16357914ab2f4ed935a
yuna-dk <149683910+yuna-dk@users.noreply.github.com>
2023-12-13 01:29:20 +0000

    [Finish] 6-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
